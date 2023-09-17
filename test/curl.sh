63b918c590132ed14ac9067f3c167bac53dd174a
Adrian Rudnik <adrian@klonmaschine.de>
2023-09-17 13:37:11 +0200

    Reworked beautify and minify options to align more to the new mjml-cli and removing the associated deprecation message

--- test/curl.sh
+++ test/curl.sh
@@ -3,10 +3,13 @@
 # Manual test to spool up the latest docker image, submit the raw body and print
 # the actual result for human review.
 
-docker run --rm -d -p 8080:80 --name mjml-server-test adrianrudnik/mjml-server:latest
+docker run --rm -p 8080:80 -e "CORS=*" --name mjml-server-test adrianrudnik/mjml-server:latest
 
 sleep 5
 
-curl --data "@raw.mjml" http://localhost:8080
+curl -v -H 'Content-Type: text/plain' --data "@raw.mjml" http://localhost:8080
 
 docker stop mjml-server-test
+
+# Alternative
+# MJML_MINIFY=true MJML_BEAUTIFY=true PORT=8080 node index.js
